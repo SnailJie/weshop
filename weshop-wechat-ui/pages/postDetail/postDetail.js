24db7212cb9e8c0d8921724ca2fe15ba29a4d1fd
Jef <snailren@126.com>
2024-11-08 09:16:04 +0800

    init

--- weshop-wechat-ui/pages/postDetail/postDetail.js
+++ weshop-wechat-ui/pages/postDetail/postDetail.js
@@ -1,41 +1,90 @@
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+const user = require('../../services/user.js');
 Page({
-  data: {
-    title: '详情页',
-    postDetail: {
-      imageUrl: "/image/疯狂暗黑圈.webp",
-      title: "玫瑰的故事🌹再次被刘亦菲的颜值所震撼❗",
-      content: "刘亦菲确实是不老女神",
-      headUrl: "/image/疯狂暗黑圈-head.webp",
-      userName: "疯狂暗黑圈",
-      likeNum: 9,
-      isLike: 1,
-      gmtCreate: "09-11",
-      commentList: [
-        { id: 1,
-          content: "你说的都对",
-          createTime: "09-11",
-          userInfo:{
-            nickname: "疯狂暗黑圈",
-            avatar: "/image/疯狂暗黑圈-head.webp",
-          },
-         
-        },
-        { id: 2,
-          content: "这个帖子是我以前看过的",
-          createTime: "09-13",
-          userInfo:{
-            nickname: "巴啦啦小魔仙",
-            avatar: "/image/疯狂暗黑圈-head.webp",
-          },
-          
-        }
-      ]
-    }
-  },
-  likePost() {  
-    console.log('------xxxlikePostxxx')
-   
-  } ,
+    data: {
+        downloadImgs: [],
+        title: '详情页',
+        postDetail: {},
+        commentList: [],
+        imageUrl: 'https://7072-prod-4gyaq2skbf5fb439-1308266486.tcb.…/kGvScXxeJB87d6d92336e44e2a1cb0c7db97d87c5ea5.jpg'
+    },
+    likePost() {
+        console.log('------xxxlikePostxxx')
+    },
+    onLoad: function (options) {
+        this.getPostDetailData();
+    },
+    getPostDetailData() {
+        let that = this;
+        let data = null;
 
-   }
-);
+        util.request(api.PostsDetail, {
+            code: "e08eaf746d9c9d40"
+        }).then(function (res) {
+            if (res.success) {
+                console.log("res.data")
+                console.log(res.data)
+                let detail = res.data
+                that.setData({
+                    postDetail: detail
+                });
+                data = detail;
+                that.downloadFiles(data.imageURL.split(';'))
+            }
+        });
+
+
+    },
+    getCommentsData() {
+        let that = this;
+        util.request(api.PostsDetail, {
+            code: "e08eaf746d9c9d40"
+        }).then(function (res) {
+            if (res.success) {
+                console.log("res.data")
+                console.log(res.data)
+                let total = res.data
+                that.setData({
+                    postDetail: total,
+                });
+            }
+        });
+    },
+    downloadFiles(fileUrls) {
+        let that = this;
+        console.log("ready to go")
+        console.log(fileUrls)
+
+        // 或如下传参
+        wx.cloud.getTempFileURL({
+            fileList: fileUrls, // 对象存储文件ID列表，最多50个，从上传文件接口或者控制台获取
+            success: res => {
+              console.log(res.fileList)
+              const fileListPreview = res.fileList.map(user => user.tempFileURL);
+             
+              console.log(fileListPreview)
+             
+              console.log('---------------xxxxx-----------')
+              that.setData({
+                downloadImgs: fileListPreview
+              })
+            },
+            fail: err => {
+                console.error(err)
+            }
+        })
+  
+    },
+    downloadFile(fileID, onCall = () => {}) {
+        wx.cloud.downloadFile({
+            fileID: fileID, // 对象存储文件ID，从上传文件接口或者控制台获取
+        }).then(res => {
+            console.log(res)
+            // resolve(res.tempFilePath)
+            return res.tempFilePath
+        }).catch(error => {
+            console.error(err)
+        })
+    }
+});
